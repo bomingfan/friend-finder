6c91a88818dd4b4ab9d07379e06610eaa535cd9f
buzzhaha <boming.fan@gmail.com>
2018-01-10 14:07:58 -0800

    start

---
+++ app.js
@@ -0,0 +1,8 @@
+// Starting dependencies.
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Setting up express to work properly
+var app = express();
+var PORT = 7000;
